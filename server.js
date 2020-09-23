bdcc0448dc8ca34ab94fb41bc766c36b048c6238
ChenCyl <programming2@163.com>
2020-09-24 02:23:53 +0800

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+// Copyright 2019 Google LLC.
+// SPDX-License-Identifier: Apache-2.0
+
+const compression = require('compression');
+const express = require('express');
+
+const app = express();
+
+app.use(compression());
+app.set('etag', false);
+
+app.use(express.static('public', {
+  // We want manual control over the caching headers.
+  cacheControl: false,
+  etag: false,
+  lastModified: false,
+}));
+
+app.get('/api/minutes', (request, response) => {
+  response.append('Cache-Control', 'max-age=1, stale-while-revalidate=5');
+  const minutes = (new Date()).getMinutes();
+  response.send({minutes});
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
